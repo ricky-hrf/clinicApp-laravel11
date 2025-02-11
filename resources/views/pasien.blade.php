57bafd136685ba9d62a0500f6a0c7db4f95e54cf
ricky-hrf <riskiharefa2000@gmail.com>
2025-02-11 23:18:47 +0700

    membuat layout pasien

---
+++ resources/views/pasien.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+@section('content')
+    <div class="card">
+        <h5 class="card-title">
+            Data Pasien
+        </h5>
+        <p>ini data pasien</p>
+    </div>
+@endsection
